c67a133e6d092ba22dfb7c80f285bf302b9f88db
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-22 10:53:44 +0530

    Popping Pages & Passing Data Back

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -87,6 +87,11 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.delete),
+          onPressed: () {
+            Navigator.of(context).pop(mealId);
+          }),
     );
   }
 }
